9438829141e67f43c6428e70596689e89313fd52
Nitho Alif Ibadurrahman <nithoalif@yahoo.com>
2015-03-19 00:40:37 +0700

    Real UnitTest

--- COLLECTION/Vector/UnitTest.cpp
+++ COLLECTION/Vector/UnitTest.cpp
@@ -4,6 +4,36 @@
 #include "VectorExp.h"
 
 int main(){
-
+	string input;
+	Vector<string> v;
+	int i;
+	cout << "Test Vector" << endl;
+	do{
+		cout << "Input Anda : ";
+		cin >> input;
+		if(input != "EXIT"){
+			v.push_back(input);
+		}
+	}while(input != "EXIT");
+	cout << "Isi Vector Anda: " << endl;
+	for(i=0; i<v.size(); i++){
+		input = v[i];
+		v.pop_back();
+		cout << input << endl;
+	}
+	v.clear();
+	cout << "Vector Anda sekarang: " << endl;
+	for(int i=0; i<v.size(); i++){
+		input = v[i];
+		v.pop_back();
+		cout << input << endl;
+	}
+	try{
+		cout << "Masukkan index yang ingin di akses : ";
+		cin >> i;
+		cout << v[i];
+	} catch(const VectorExp&){
+		cout << "Index out of bounds!" << endl;
+	}
 	return 0;
 }
