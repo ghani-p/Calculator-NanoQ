d41788d082ff7ec9f3b003d5d030a8f1546c2c60
Fiqie Astahta <fiqi9e@gmail.com>
2015-03-18 17:04:27 +0700

    Update Romawi.h

--- NUMBER/Romawi/Romawi.h
+++ NUMBER/Romawi/Romawi.h
@@ -6,28 +6,109 @@
 #include "../Arab/Arab.h"
 #include <string>
 using namespace std;
+
+/*! Sebuah kelas romawi turunan dari kelas arab */
 class Romawi:public Arab{
 public:
-    //ctor
+
+    /**
+    *Konstruktor tanpa parameter
+    */
     Romawi();
+    
+    /**
+    *Konstruktor dengan parameter
+    *I.S. string input berupa string simbol angka Romawi dengan nilai antara I hingga MMMCMXCIX
+    *F.S. menyimpan kedalam variabel nilai angka integer dan variabel nromawi angka romawi
+    *@param A untuk parameter pertama
+    */
     Romawi(string A);
-    //dtor
+    
+    /**
+    *Destruktor
+    */
     virtual ~Romawi();
-    //cctor
+    
+    /**
+    *Copy Konstruktor
+    *@param R untuk parameter pertama
+    */
     Romawi(const Romawi& R);
+    
+    /**
+    *Operator=
+    *@param R untuk parameter pertama
+    */
     Romawi& operator=(const Romawi& R);
-    //overide
+    
+    /**
+    *fungsi prosedur penjumlahan override dari Arab
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    * F.S. angka Romawi hasil penjumlahan tidak melebihi MMMCMXCIX
+    *@param A untuk parameter pertama
+    */
     virtual void operator+(const Operand& A);
+    
+    /**
+    *fungsi prosedur pengurangan override dari Arab
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    * F.S. angka Romawi hasil pengurangan tidak kurang dari I
+    *@param A untuk parameter pertama
+    */
     virtual void operator-(const Operand& A);
+    
+    /**
+    *fungsi prosedur perkalian override dari Arab
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    * F.S. angka Romawi hasil perkalian tidak melebihi MMMCMXCIX
+    *@param A untuk parameter pertama
+    */
     virtual void operator*(const Operand& A);
+    
+    /**
+    *fungsi prosedur pembagian override dari Arab
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    *      angka Romawi pembagi tidak boleh 0
+    * F.S. angka Romawi hasil pembagian tidak kurang dari I
+    *@param A untuk parameter pertama
+    */
     virtual void operator/(const Operand& A);
+    
+    /**
+    *fungsi prosedur modulo override dari Arab
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    * F.S. angka Romawi hasil modulo tidak kurang dari I
+    *@param A untuk parameter pertama
+    */
     virtual void operator%(const Operand& A);
+    
+    /**
+    *fungsi prosedur konversi angka Romawi menjadi angka Arab
+    * assert untuk fungsi prosedur kalkulasi
+    * assert precondition
+    * I.S. angka Romawi antara I hingga MMMCMXCIX
+    * F.S. nilai di isi dengan nilai hasil konversi angka Romawi
+    *@param R untuk parameter pertama
+    */
     void ConvertToArab(string R);
+    
+    /**
+    *fungsi prosedur konversi angka Arab ke angka Romawi
+    * assert untuk fungsi prosedur kalkulasi
+    * assert postcondition
+    * I.S. angka Arab dari 1 hingga 3999
+    * F.S. Nromawi di isi dengan nilai hasil konversi angka Arab
+    *@param A untuk parameter pertama
+    */
     void ConvertToRomawi(int A);
+    
+    /**
+    *fgetter
+    */
     string getRomawi()const;
 
 private:
-    string Nromawi;
+    string Nromawi;/*!< sebuah string Romawi */
 };
 
 #endif // ROMAWI_H_INCLUDED
