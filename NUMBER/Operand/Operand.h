cf64778e8a9bd84b985edc1a2106fc2538ba147e
Fiqie Astahta <fiqi9e@gmail.com>
2015-03-18 16:16:59 +0700

    Update Operand.h

--- NUMBER/Operand/Operand.h
+++ NUMBER/Operand/Operand.h
@@ -3,22 +3,75 @@
 #ifndef OPERAND_H_INCLUDED
 #define OPERAND_H_INCLUDED
 #include <cassert>
-//Abstract class
+/*! Sebuah kelas abstrak Operand */
 class Operand{
 public:
-    //arabic operators
+    /**
+    *fungsi prosedure penjumlahan operand
+    * assert untuk operator penjumlahan pada bilangan logika
+    * @param O untuk parameter pertama
+    */
     virtual void operator+(const Operand& O);
+    
+    /**
+    *fungsi prosedure pembagian operand
+    * assert untuk operator pembagian pada bilangan logika
+    * @param O untuk parameter pertama
+    */
     virtual void operator/(const Operand& O);
+    
+    /**
+    *fungsi prosedure pengurangan operand
+    * assert untuk operator pengurangan pada bilangan logika
+    * @param O untuk parameter pertama
+    */
     virtual void operator-(const Operand& O);
+    
+    /**
+    *fungsi prosedure perkalian operand
+    * assert untuk operator perkalian pada bilangan logika
+    * @param O untuk parameter pertama
+    */
     virtual void operator*(const Operand& O);
+    
+    /**
+    *fungsi prosedure modulo operand
+    * assert untuk operator modulo pada bilangan logika
+    * @param O untuk parameter pertama
+    */
     virtual void operator%(const Operand& O);
 
-    //logical operators
+    /**
+    *fungsi prosedure not operand
+    * assert untuk operator not pada bilangan arab
+    */
     virtual void operatornot();
+    
+    /**
+    *fungsi prosedure and operand
+    * assert untuk operator and pada bilangan arab
+    * @param O untuk parameter pertama
+    */
     virtual void operatorand(const Operand& O);
+    
+    /**
+    *fungsi prosedure or operand
+    * assert untuk operator or pada bilangan arab
+    * @param O untuk parameter pertama
+    */
     virtual void operatoror(const Operand& O);
+    
+    /**
+    *fungsi prosedure xor operand
+    * assert untuk operator xor pada bilangan arab
+    * @param O untuk parameter pertama
+    */
     virtual void operatorxor(const Operand& O);
-    //pure virtual
+    
+    /**
+    *fungsi get nilai 
+    * pure virtual
+    */
     virtual int getNilai()const=0;
 };
 
