df5659d26584f2b8d194bcf127021c323b721eac
Nitho Alif Ibadurrahman <nithoalif@yahoo.com>
2015-03-19 00:47:41 +0700

    Real UnitTest

--- COLLECTION/Stack/UnitTest.cpp
+++ COLLECTION/Stack/UnitTest.cpp
@@ -4,6 +4,22 @@
 #include "StackExp.h"
 
 int main(){
-
+	string input;
+	Stack<string> s;
+	int i;
+	
+	cout << "Test Stack" << endl;
+	do{
+		cout << "Input Anda : ";
+		cin >> input;
+		if(input != "EXIT"){
+			s.push(input);
+		}
+	}while(input != "EXIT");
+	cout << "Stack Anda sekarang: " << endl;
+	while(!s.empty()){
+		cout << s.top() << endl;
+		s.pop();
+	}
 	return 0;
 }
