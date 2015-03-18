1a1955063c4e5a269d838dd89789325ccd2574a6
Fiqie Astahta <fiqi9e@gmail.com>
2015-03-18 22:54:37 +0700

    Update Perintah.h

--- COMMAND/Perintah/Perintah.h
+++ COMMAND/Perintah/Perintah.h
@@ -5,30 +5,72 @@
 
 #include "../../MEMORY/Memori/Memori.h"
 using namespace std;
-
+/*! Sebuah kelas perintah */
 class Perintah{
 public:
-	//CTOR
+	/**
+    	*Konstruktor default
+	 */
 	Perintah();
 
-	//COMMANDS
+	/**
+    	*mengembalikan perintah yang tersimpan dalam memory sebanyak n
+    	* @param n untuk parameter pertama
+    	*/
 	void UNDO(int n);
+	
+	/**
+    	*mengembalikan perintah undo sebanyak n
+    	*  @param n untuk parameter pertama
+    	*/
 	void REDO(int n);
+	
+	/**
+	*menampilkan memori sebanyak n terakhir
+	* @param n untuk parameter pertama
+	*/
 	void SHOWMEM(int n);
+	
+	/**
+	*menampilkan seluruh memori yang ada
+	*/
 	void SHOWALL();
+	
+	/**
+	*mengatur setting mode kalkulator
+	*/
 	void SETTING();
+	/**
+	*menyimpan kedalam memory
+	* @param file untuk parameter pertama
+	*/
 	void SAVE(string file);
+	/**
+	*keluar dari program
+	*/
 	void EXIT();
 	
-	//GETTER
+	/**
+	*getter
+	* mengembalikan string ekspresi
+	*/
 	string getEkspresi();
+	
+	/**
+	*getter
+	* mengembalikan string bilangan
+	*/
 	string getBilangan();
+	
+	/**
+	*getter memori
+	*/
 	Memori& getMemori();
 
 private:
-	Memori memory;
-	string ekspresi;
-	string bilangan;
+	Memori memory; /*! variabel memori */
+	string ekspresi; /*! variabel ekspresi*/
+	string bilangan; /*! variabel bilangan */
 };
 
 
